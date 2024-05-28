449ee1466ea5619871c02b71cce25ded5b93e0ec
Michael Gorbunov <mg0rby@yandex.ru>
2024-05-28 19:49:03 +0300

    fix

--- src/hw_11_1.py
+++ src/hw_11_1.py
@@ -15,3 +15,9 @@ def non_empty_truths(val: list[Any]) -> list[Any]:
 #     return list(map(f, xs))
 #
 # my_map("lambda x: x + 2", [-1, 2, -3])
+# print(lambda x: x + 2, [-1, 2, -3])
+# list(my_map(lambda x: x + 2, [-1, 2, -3]))  # [1, 4, -1]
+# list(my_filter(lambda x: x % 2 == 1, range(10)))  # [1, 3, 5, 7, 9]
+# list(replicate_each(1, [1, 'a']))  # [1, 'a']
+# list(replicate_each(3, [1, 'a']))  # [1, 1, 1, 'a', 'a', 'a']
+# list(replicate_each(0, [1, 'a']))  # []
