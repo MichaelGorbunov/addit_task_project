ca468accf028d0246ec459eae0c0dff4224b9190
Michael Gorbunov <mg0rby@yandex.ru>
2024-05-28 19:41:13 +0300

    non_empty_truths

--- src/hw_11_1.py
+++ src/hw_11_1.py
@@ -1,6 +1,17 @@
-def non_empty_truths(val: list) -> list:
+from typing import Any
+
+
+def non_empty_truths(val: list[Any]) -> list[Any]:
+    """функция очищает список от пустых,нулеых, и false значений"""
     if not isinstance(val, list):
         return val
 
     return [x for x in map(non_empty_truths, val) if x]
+
+
 #
+# y=(lambda x: x + 2, [-1, 2, -3])
+# def my_map(f:str, xs:list):
+#     return list(map(f, xs))
+#
+# my_map("lambda x: x + 2", [-1, 2, -3])
